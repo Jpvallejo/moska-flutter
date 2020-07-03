7a5bfbad942924a0d89c483da6a285feeda075bf
Juan Pablo Vallejo <jvallejo@solstice.com>
2020-07-03 14:56:42 -0300

    Add transaction models

---
+++ lib/src/models/expense_model.dart
@@ -0,0 +1,5 @@
+import 'package:moska_app/src/models/transaction_model.dart';
+
+class ExpenseModel extends TransactionModel {
+  String accountId;
+}
---
+++ lib/src/models/income_model.dart
@@ -0,0 +1,5 @@
+import 'package:moska_app/src/models/transaction_model.dart';
+
+class IncomeModel extends TransactionModel {
+  String accountId;
+}
