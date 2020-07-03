2ce2d7b3f4a97f1ce473929f2358765f94748a02
Juan Pablo Vallejo <jvallejo@solstice.com>
2020-07-03 14:56:31 -0300

    Add card amount to view

--- lib/src/ui/credit_cards_screen.dart
+++ lib/src/ui/credit_cards_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:moska_app/src/models/credit_card_view_model.dart';
 import 'package:moska_app/src/services/credit_card_expense_service.dart';
 import 'package:moska_app/src/services/credit_card_service.dart';
@@ -14,72 +15,86 @@ class CreditCardsScreen extends StatefulWidget {
 class _CreditCardsScreenState extends State<CreditCardsScreen> {
   List<ListTile> rows;
   List<String> ids;
+  final oCcy = new NumberFormat("#,##0.00", "en_US");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: FutureBuilder<List<CreditCardViewModel>>(
-          future: getCreditCards(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState != ConnectionState.done) {
-              return Text("loading");
-            } else if (snapshot.hasError) {
-              if (snapshot.error is UnauthorizedException) {
-                MyNavigator.goToLogin(context);
-              }
-              return Text(snapshot.error.toString());
-            } else {
-              print(snapshot.data);
-              rows = snapshot.data
-                  .map((cc) => ListTile(
-                        leading: GestureDetector(
-                          onTap: () {
-                            MyNavigator.goToCCExpenses(context, cc);
-                          },
-                          child: Icon(Icons.credit_card),
-                        ),
-                        title: GestureDetector(
+        body: Column(
+      children: <Widget>[
+        ListTile(
+            title: Text(
+          DateFormat(DateFormat.MONTH).format(DateTime.now()),
+          textAlign: TextAlign.center,
+        )),
+        SingleChildScrollView(
+          child: FutureBuilder<List<CreditCardViewModel>>(
+              future: getCreditCards(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState != ConnectionState.done) {
+                  return Text("loading");
+                } else if (snapshot.hasError) {
+                  if (snapshot.error is UnauthorizedException) {
+                    MyNavigator.goToLogin(context);
+                  }
+                  return Text(snapshot.error.toString());
+                } else {
+                  print(snapshot.data);
+                  rows = snapshot.data
+                      .map((cc) => ListTile(
+                          leading: GestureDetector(
                             onTap: () {
                               MyNavigator.goToCCExpenses(context, cc);
                             },
-                            child: Text(cc.name)),
-                      ))
-                  .toList();
-                print(rows);
-              return Column(children: rows);
-            }
-          }),
-    ),
-    // bottomNavigationBar: BottomAppBar(
-    //     child: FutureBuilder<double>(
-    //       future: getCCExpensesTotalSum(ids),
-    //       builder: (context, snapshot) {
-    //         if (snapshot.connectionState != ConnectionState.done) {
-    //           return Text("loading");
-    //         } else if (snapshot.hasError) {
-    //           if (snapshot.error is UnauthorizedException) {
-    //             MyNavigator.goToLogin(context);
-    //           }
-    //           return Text(snapshot.error.toString());
-    //         } else {
-    //           return Row(
-    //             mainAxisSize: MainAxisSize.max,
-    //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //             children: [
-    //               Padding(
-    //                 padding: const EdgeInsets.all(15.0),
-    //                 child: Text("Total: "),
-    //               ),
-    //               Padding(
-    //                   padding: const EdgeInsets.all(15.0),
-    //                   child: Text('')),
-    //             ],
-    //           );
-    //         }
-    //       },
-    //     ),
-    //     color: Colors.grey,
-    //   ),
-    );
+                            child: Icon(Icons.credit_card),
+                          ),
+                          title: GestureDetector(
+                              onTap: () {
+                                MyNavigator.goToCCExpenses(context, cc);
+                              },
+                              child: Text(cc.name)),
+                          trailing: GestureDetector(
+                              onTap: () {
+                                MyNavigator.goToCCExpenses(context, cc);
+                              },
+                              child: Text(oCcy.format(cc.totalAmount)))))
+                      .toList();
+                  print(rows);
+                  return Column(children: rows);
+                }
+              }),
+        ),
+      ],
+    )
+        // bottomNavigationBar: BottomAppBar(
+        //     child: FutureBuilder<double>(
+        //       future: getCCExpensesTotalSum(ids),
+        //       builder: (context, snapshot) {
+        //         if (snapshot.connectionState != ConnectionState.done) {
+        //           return Text("loading");
+        //         } else if (snapshot.hasError) {
+        //           if (snapshot.error is UnauthorizedException) {
+        //             MyNavigator.goToLogin(context);
+        //           }
+        //           return Text(snapshot.error.toString());
+        //         } else {
+        //           return Row(
+        //             mainAxisSize: MainAxisSize.max,
+        //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //             children: [
+        //               Padding(
+        //                 padding: const EdgeInsets.all(15.0),
+        //                 child: Text("Total: "),
+        //               ),
+        //               Padding(
+        //                   padding: const EdgeInsets.all(15.0),
+        //                   child: Text('')),
+        //             ],
+        //           );
+        //         }
+        //       },
+        //     ),
+        //     color: Colors.grey,
+        //   ),
+        );
   }
 }
